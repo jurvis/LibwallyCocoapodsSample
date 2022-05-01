038c4ff9eae6f4b83d876c8cf6ee8a3ba5df8170
Jurvis Tan <5944973+jurvis@users.noreply.github.com>
2022-04-30 18:07:38 -0700

    Initial Commit

---
+++ LibWallyCocoapodsSample/LibWallyCocoapodsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibWallyCocoapodsSampleApp.swift
+//  LibWallyCocoapodsSample
+//
+//  Created by Jurvis on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LibWallyCocoapodsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
